4c32d691c1702f5dbc05ce69cd2fb6756a700d90
Jim Hough <teachlife@outlook.com>
2019-05-30 02:15:27 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -66,4 +66,12 @@ class Dog
   def self.new_from_db (row)
     new_song = self.new(id: row[0], name: row[1], breed: row[2])
   end
+
+  def self.find_by_name(name)
+    sql = 'SELECT * FROM dogs WHERE name = ? LIMIT 1'
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+    
 end
