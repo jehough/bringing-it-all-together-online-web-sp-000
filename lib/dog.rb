fc94afc6c14c9bc5e6f8e680b7459950460f7693
Jim Hough <teachlife@outlook.com>
2019-05-30 01:38:29 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,25 @@
+class Dog
+  attr_accessor :name, :breed
+  attr_reader :id
+
+  def initialize(name:, breed:, id: nil)
+    @name = name
+    @breed = breed
+  end
+
+  def self.create_table
+    sql = <<-SQL
+    CREATE TABLE dogs(
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      breed TEXT
+    );
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    DB[:conn].execute('DROP TABLE dogs')
+  end
+
+end
